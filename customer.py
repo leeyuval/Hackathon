6b4bacba5611f9c8d988dfe1b3c034740b3a9a5b
omerkasher <omer.kasher@mail.huji.ac.il>
2022-04-07 12:33:19 +0300

    omer's commit

--- customer.py
+++ customer.py
@@ -1,3 +1,15 @@
 from kivy.support import install_twisted_reactor
 
-class Customer
+class Customer:
+    def __init__(self, name, phone_num, address, cus_num):
+        self.__name = None
+        self.__phone_num = None
+        self.__address = None
+        self.__customer_num = None
+
+
+
+
+
+
+
--- main.py
+++ main.py
@@ -1,18 +1,59 @@
-# This is a sample Python script.
+# from kivy.support import install_twisted_reactor
+# from kivy.app import App
+# from kivymd.app import MDApp
+# from kivy.uix.textinput import TextInput
+# from kivy.uix.button import Button
+# from kivy.uix.gridlayout import GridLayout
+# from kivy.uix.widget import Widget
+#
+#
+# class MainApp(App):
+#     def build(self):
+#         """getting info from user"""
+#
+#         # layout = GridLayout(cols=3)
+#         name = TextInput(text='Enter your name')
+#         phone_num = TextInput(text='Enter your phone number')
+#         address = TextInput(text='Enter your address')
+#         submit = Button(text='Submit',
+#                         on_press=self.submit(name, phone_num, address))
+#         # layout.add_widget(name)
+#         # layout.add_widget(phone_num)
+#         # layout.add_widget(address)
+#         # layout.add_widget(submit)
+#         return name
+#
+#     def submit(self, name, phone_num, address):
+#         print(name.text, phone_num.text, address.text)
+#
+#
+# if __name__ == '__main__':
+#     MainApp().run()
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from kivymd.app import MDApp
+from kivymd.uix.label import MDLabel, MDIcon
+from kivymd.font_definitions import theme_font_styles
+from kivymd.app import MDApp
+from kivymd.uix.screen import Screen
+from kivymd.uix.textfield import MDTextField
 
-from kivy.support import install_twisted_reactor
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import helpers
+from kivy.lang import Builder
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('omer')
-    print("These are Yuval's new changes")
+class DemoApp(MDApp):
+    def build(self):
+        self.theme_cls.primary_palette="Green"
+        screen = Screen()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        username = MDTextField(
+            pos_hint={'center_x': 0.5, 'center_y': 0.5},
+            size_hint_x=None, width=200)
+
+        username = Builder.load_string(helpers.username_input)
+        screen.add_widget(username)
+        print(username.text)
+        return screen
+
+
+DemoApp().run()
